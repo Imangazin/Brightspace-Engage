d756aaa837096d58ac05719228e60f95680107da
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-11-17 09:40:49 -0500

    Add files via upload

---
+++ src/info.php
@@ -0,0 +1,19 @@
+<?php
+//Need to change 
+$config = array(
+    'host'       => '',
+    'port'       => 443,
+    'scheme'     => 'https',
+
+    'appId'      => '',
+    'appKey'     => '',
+    'userId'     => '',
+    'userKey'    => '',
+
+    'LP_Version' => '',
+
+    'eventAuthToken' => '',
+    'eventUrl'       => ''
+);
+?>
+
