2ae862dc0437a31e7d7fd5c5ebdfe041d5310c91
Nurbek Imangazin <nurbekimangazin@gmail.com>
2023-11-21 10:30:23 -0500

    onload

--- js/ble-bu.js
+++ js/ble-bu.js
@@ -9,18 +9,6 @@ $(document).ready(function() {
     allowClear: true, // Option to clear selection
     width: '100%', // Adjust the width as needed
   });
-
-  $.post('src/experienceBU.php', function (data) {
-    data = JSON.parse(data); 
-    data.forEach(function(each){
-      const optionElement = document.createElement("option");
-      optionElement.value = each.id;
-      optionElement.text = each.name;
-      orgSelectTag.appendChild(optionElement);
-    });
-  }).fail(function (xhr, status, error) {
-    console.error('GET request failed:', status, error);
-  });
 });
 
 // Add an event listener to the checkbox
@@ -37,4 +25,16 @@ orgSelectTag.addEventListener("change", function(event){
   console.log(selectedValue);
 });
 
-
+window.addEventListener('load', function () {
+  $.get('src/experienceBU.php', function (data) {
+    data = JSON.parse(data); 
+    data.forEach(function(each){
+      const optionElement = document.createElement("option");
+      optionElement.value = each.id;
+      optionElement.text = each.name;
+      orgSelectTag.appendChild(optionElement);
+    });
+  }).fail(function (xhr, status, error) {
+    console.error('GET request failed:', status, error);
+  });
+});
